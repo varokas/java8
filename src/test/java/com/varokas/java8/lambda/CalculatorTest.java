16a25078edaa4f4b04dfe6946826bca6129f4705
Varokas Panusuwan <varokas@panusuwan.com>
2014-03-18 22:34:34 -0700

    format to two spaces

--- src/test/java/com/varokas/java8/lambda/CalculatorTest.java
+++ src/test/java/com/varokas/java8/lambda/CalculatorTest.java
@@ -9,31 +9,31 @@ import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    private Calculator calculator;
+  private Calculator calculator;
 
-    @Before
-    public void setUp() throws Exception {
-        calculator = new Calculator();
-    }
+  @Before
+  public void setUp() throws Exception {
+    calculator = new Calculator();
+  }
 
-    @Test
-    public void testLambda() throws Exception {
-        assertEquals(3, Calculator.ADD.op(1,2));
-        assertEquals(6, Calculator.SUBTRACT.op(10,4));
-        assertEquals(8, Calculator.MULTIPLY.op(4,2));
-        assertEquals(2, Calculator.DIVIDE.op(6,3));
-    }
+  @Test
+  public void testLambda() throws Exception {
+    assertEquals(3, Calculator.ADD.op(1, 2));
+    assertEquals(6, Calculator.SUBTRACT.op(10, 4));
+    assertEquals(8, Calculator.MULTIPLY.op(4, 2));
+    assertEquals(2, Calculator.DIVIDE.op(6, 3));
+  }
 
-    @Test
-    public void testCalculate() throws Exception {
-        Calculator.Operator mockOperator = Mockito.mock(Calculator.Operator.class);
+  @Test
+  public void testCalculate() throws Exception {
+    Calculator.Operator mockOperator = Mockito.mock(Calculator.Operator.class);
 
-        int a = RandomUtils.nextInt();
-        int b = RandomUtils.nextInt();
-        int result = RandomUtils.nextInt();
+    int a = RandomUtils.nextInt();
+    int b = RandomUtils.nextInt();
+    int result = RandomUtils.nextInt();
 
-        Mockito.when(mockOperator.op(a,b)).thenReturn(result);
+    Mockito.when(mockOperator.op(a, b)).thenReturn(result);
 
-        assertEquals(result, calculator.calculate(a,b, mockOperator));
-    }
+    assertEquals(result, calculator.calculate(a, b, mockOperator));
+  }
 }
